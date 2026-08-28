8c020a67fed78251ee4704a9ca354d84cad0ca22
acebenyahya-arch <acebenyahya@gmail.com>
2026-08-28 01:52:46 +0100

    Update WhatsappButton.tsx

--- src/components/site/WhatsappButton.tsx
+++ src/components/site/WhatsappButton.tsx
@@ -16,7 +16,7 @@ export function WhatsappButton({
 
   return (
     <a
-      href="https://wa.me/‪212784092681"
+      href="https://wa.me/212784092681"
       target="_blank"
       rel="noreferrer"
       className={`${base} ${styles}`}
